72488e7a1fbd8cb51a27796a41bf075cfb9fe676
Lafaiete do Vale <lafa.vale@gmail.com>
2023-04-24 01:08:08 -0300

    SignOut broadcast to all pages

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -6,7 +6,7 @@ import { setupAPIClient } from "@/services/api";
 import { Can } from "@/components/Can";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
+  const { user, signOut, isAuthenticated } = useContext(AuthContext);
 
   useEffect(() => {
     api
@@ -18,6 +18,9 @@ export default function Dashboard() {
   return (
     <>
       <h1>Dashboard: {user?.email}</h1>
+
+      <button onClick={signOut}>Sign out</button>
+
       <Can permissions={["metrics.list"]}>
         <div>Métricas</div>
       </Can>
